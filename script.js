7ac4f649cf7ab41b6d65c1f0d64162fc109c997a
pawaters <pierre.alban.waters@gmail.com>
2023-06-08 11:37:19 +0300

    now with iphone calculator colors

--- script.js
+++ script.js
@@ -3,33 +3,57 @@ let buttons = Array.from(document.getElementsByClassName('buttons'));
 let operator = null;
 let operand1 = null;
 let operand2 = null;
+let resultShown = false;
+
+function clear() {
+    display.value = "";
+    operator = null;
+    operand1 = null;
+    operand2 = null;
+    resultShown = false;
+}
+
+function deleteLast() {
+    display.value = display.value.slice(0, -1);
+}
+
+function appendNumber(number) {
+    if(resultShown) {
+        clear();
+    }
+    display.value += number;
+}
+
+function chooseOperation(operation) {
+    operator = operation;
+    operand1 = parseFloat(display.value);
+    display.value = "";
+}
+
+function compute() {
+    operand2 = parseFloat(display.value);
+    let result = eval(operand1 + ' ' + operator + ' ' + operand2);
+    display.value = result;
+    operator = null;
+    operand1 = result;
+    operand2 = null;
+    resultShown = true;
+}
 
 buttons.map( button => {
     button.addEventListener('click', (e) => {
         let buttonText = e.target.innerText;
-        console.log(buttonText);
         if(buttonText >= '0' && buttonText <= '9'){
-            if(display.value === null){
-                display.value = buttonText;
-            } else {
-                display.value += buttonText;
-            }
+            appendNumber(buttonText);
         } else if(buttonText === 'C'){
-            display.value = "";
-            operator = null;
-            operand1 = null;
-            operand2 = null;
+            clear();
+        } else if(buttonText === 'Del'){
+            deleteLast();
         } else if(buttonText === '+' || buttonText === '-' || buttonText === '*' || buttonText === '/'){
-            operator = buttonText;
-            operand1 = parseFloat(display.value);
-            display.value = "";
+            chooseOperation(buttonText);
         } else if(buttonText === '='){
-            operand2 = parseFloat(display.value);
-            let result = eval(operand1 + ' ' + operator + ' ' + operand2);
-            display.value = result;
-            operator = null;
-            operand1 = result;
-            operand2 = null;
+            compute();
         }
     });
 });
+
