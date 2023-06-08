039966cc5fcd58dcac68ce8b52b65de333149cee
pawaters <pierre.alban.waters@gmail.com>
2023-06-08 12:06:22 +0300

    added automatic tests in colose log

--- script.js
+++ script.js
@@ -24,25 +24,39 @@ function appendNumber(number) {
     display.value += number;
 }
 
-function chooseOperation(operation) {
-    operator = operation;
-    operand1 = parseFloat(display.value);
-    display.value = "";
+function chooseOperation(oper) {
+    if (!resultShown) {
+        operator = oper;
+        operand1 = parseFloat(display.value);
+        display.value = "";
+    }
 }
 
 function compute() {
     operand2 = parseFloat(display.value);
-    let result = eval(operand1 + ' ' + operator + ' ' + operand2);
-    display.value = result;
-    operator = null;
-    operand1 = result;
-    operand2 = null;
-    resultShown = true;
+    if(operator === '/' && operand2 === 0) {
+        display.value = 'Error: Divide by 0';
+        operator = null;
+        operand1 = null;
+        operand2 = null;
+        resultShown = true;
+    } else {
+        let result = eval(operand1 + ' ' + operator + ' ' + operand2);
+        result = Math.round(result * 100) / 100;
+        display.value = result;
+        operator = null;
+        operand1 = result;
+        operand2 = null;
+        resultShown = true;
+    }
 }
 
 buttons.map( button => {
     button.addEventListener('click', (e) => {
         let buttonText = e.target.innerText;
+        if(resultShown && !isNaN(buttonText)){
+            clear();
+        }
         if(buttonText >= '0' && buttonText <= '9'){
             appendNumber(buttonText);
         } else if(buttonText === 'C'){
@@ -57,3 +71,45 @@ buttons.map( button => {
     });
 });
 
+// TESTS
+document.getElementById('test-button').addEventListener('click', runTests);
+
+function runTests() {
+    // Test 1
+    clear();
+    appendNumber('5');
+    chooseOperation('+');
+    appendNumber('3');
+    compute();
+    console.assert(display.value == '8', "Test 1 (5 + 3 = 8) Failed");
+    console.log("Test 1 (5 + 3 = 8) Passed");
+
+    // Test 2
+    clear();
+    appendNumber('6');
+    chooseOperation('-');
+    appendNumber('1');
+    compute();
+    console.assert(display.value == '5', "Test 2 (6 - 1 = 5) Failed");
+    console.log("Test 2 (6 - 1 = 5) Passed");
+
+    // Test 3
+    clear();
+    appendNumber('9');
+    chooseOperation('/');
+    appendNumber('0');
+    compute();
+    console.assert(display.value == 'Error: Divide by 0', "Test 3 (9 / 0 = Error: Divide by 0) Failed");
+    console.log("Test 3 (9 / 0 = Error: Divide by 0) Passed");
+
+    // Test 4
+    clear();
+    appendNumber('0.1');
+    chooseOperation('+');
+    appendNumber('0.2');
+    compute();
+    console.assert(display.value == '0.3', "Test 4 (0.1 + 0.2 = 0.3) Failed");
+    console.log("Test 4 (0.1 + 0.2 = 0.3) Passed");
+
+    console.log("All tests passed successfully!");
+}
