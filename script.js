6166010228bc8f0a28e377a51335b6aacd5b684b
pawaters <pierre.alban.waters@gmail.com>
2023-06-09 15:01:57 +0300

    scrolling solved

--- script.js
+++ script.js
@@ -125,6 +125,27 @@ window.addEventListener('keydown', (e) => {
 document.getElementById('test-button').addEventListener('click', runTests);
 const resultDiv = document.getElementById('test-results');  
 
+function handleTests() {
+    let testButton = document.getElementById('test-button');
+    let clearTestButton = document.getElementById('clear-test-button');
+    if (testButton.style.display !== 'none') {
+        runTests();
+        testButton.style.display = 'none';
+        clearTestButton.style.display = 'inline-block';
+    } 
+}
+
+function clearTests() {
+    let testButton = document.getElementById('test-button');
+    let clearTestButton = document.getElementById('clear-test-button');
+    let testTable = document.getElementById('test-table');
+    let tableBody = document.getElementById('table-body');
+    tableBody.innerHTML = ""; // clear previous test results
+    testTable.style.display = "none"; // hide the table
+    clearTestButton.style.display = 'none';
+    testButton.style.display = 'inline-block';
+}
+
 function runTest(commands, expectedResult) {
     clear();
     let testDescription = '';
@@ -149,12 +170,28 @@ function runTest(commands, expectedResult) {
         }
     }
     const result = display.value == expectedResult ? "Passed" : "Failed";
-    resultDiv.innerHTML += `${testDescription} Expected: ${expectedResult} Output: ${display.value} --> ${result}<br>`;
+    let tableBody = document.getElementById('table-body');
+    let row = tableBody.insertRow();
+    row.insertCell(0).innerHTML = testDescription;
+    row.insertCell(1).innerHTML = expectedResult;
+    row.insertCell(2).innerHTML = display.value;
+    row.insertCell(3).innerHTML = result;
 }
 
 function runTests() {
-    resultDiv.innerHTML = ""; // clear previous test results
-    runTest([4, '+', 2, '-', 2, '/', 2, '*', 2, '='], '4');
-    runTest([4, '+', '+', 2, '=', '-', 2, '/', 2, '*', 2, '='], '4');
+    let testTable = document.getElementById('test-table');
+    let tableBody = document.getElementById('table-body');
+    tableBody.innerHTML = ""; // clear previous test results
+    testTable.style.display = "block"; // make the table visible
+    runTest([4, '+', 2, '-', 2, '/', 4, '*', 2, '='], '2');
+    runTest([4, '-/+', '+', 2, '='], '-2');
+    runTest(['2,0005', '+', '1,0005', '='], '3.0005');
+    runTest([8, '%', '=', '*', 2, '='], '0.16');
+    runTest([1, '/', 0, '='], 'Error: Cannot Divide by 0');
+    runTest([1, '+', '0,1', '=', '+', '0,2', '='], '1.3');
+    runTest([1, '+', '+', 2, '='], '3.000');
+    runTest([1, '+', 2, '+', 3, '+', 4, '='], '10.000');
+    runTest([1, '+', 2, '*', 3, '='], '9.000');
+    runTest(['1,1', '+', '2,2', '='], '3.300');
 }
 
