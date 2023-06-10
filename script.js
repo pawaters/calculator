0ba10de491ace3209e173e00960137a080f06e92
pawaters <pierre.alban.waters@gmail.com>
2023-06-10 15:25:26 +0300

    responsive styling for mobile added

--- script.js
+++ script.js
@@ -169,7 +169,7 @@ buttons.forEach(button => {
                         appendNumber(buttonValue);
                     }
                     break;
-            }
+            }  
         }
     });
 });
